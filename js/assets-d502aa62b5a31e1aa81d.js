9db88f7f694b5ac900e97a2685a6026aebc2d8e7
vasishthg <64731619+vasishthg@users.noreply.github.com>
2021-01-31 20:37:28 +0530

    Update

---
+++ js/assets-d502aa62b5a31e1aa81d.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Error</title>
+</head>
+<body>
+<pre>Cannot GET /assets/d502aa62b5a31e1aa81d.js</pre>
+</body>
+</html>
---
+++ js/assets-d9c308b4011ffd30ae32.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Error</title>
+</head>
+<body>
+<pre>Cannot GET /assets/d9c308b4011ffd30ae32.js</pre>
+</body>
+</html>
